bdd6746bd26babe5312dd317e19bec8430f2af4d
Md Aamir Raza <amir78692sohel@gmail.com>
2024-12-12 12:30:54 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ const handleSignup=async (e)=>{
 
     }
     try{
-        const url="https://auth-mern-chi.vercel.app/auth/signup";
+        const url="https://auth-mern-three.vercel.app/auth/signup";
         const response=await fetch(url,{
             method:"POST",
             headers:{
